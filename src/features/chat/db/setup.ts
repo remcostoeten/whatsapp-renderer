4b0c6670972317b8e31e49811dfe16aa6a365eee
Remco Stoeten <remcostoeten@hotmail.com>
2024-11-06 21:41:43 +0100

    refactor: rm stuff notneeded

--- src/features/chat/db/setup.ts
+++
@@ -1,9 +0,0 @@
-import { seedChats } from './seed'
-
-async function setup() {
-	console.log('Starting database setup...')
-	await seedChats()
-	console.log('Database setup completed')
-}
-
-setup().catch(console.error)
